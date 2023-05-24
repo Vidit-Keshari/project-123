c34306e595ee220de57868f5b790e4d03b778c0c
Vidit Keshari <95676105+Vidit-Keshari@users.noreply.github.com>
2023-05-24 19:02:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var text = "This is developed by The developer VIDIT KESHARI";
+var text_2 = "This is developed by The developer VIDIT KESHARI and this app name is FONT MANIPULATOR, you can have fun in controlling text";
+function developer() {
+    window.alert(text);
+}
+
+function app() {
+    window.alert(text_2);
+}
